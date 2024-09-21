082d94e5a351347e9f7097d5337d2a0e3b1ef47f
VS <sofronov.vit@gmail.com>
2024-09-21 13:10:55 +0400

    add initial data load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { TuiRoot } from '@taiga-ui/core';
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '@components/header/header.component';
+import { Store } from '@ngrx/store';
+import { TagActions } from '@store/tags/tag.actions';
+import { NoticeActions } from '@store/notices/notice.actions';
+import { ReminderActions } from '@store/reminders/reminder.actions';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +14,13 @@ import { HeaderComponent } from '@components/header/header.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.less',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test-paradox';
+  private readonly store = inject(Store);
+
+  ngOnInit() {
+    this.store.dispatch(NoticeActions.fetchNotices());
+    this.store.dispatch(ReminderActions.fetchReminders());
+    this.store.dispatch(TagActions.fetchTags());
+  }
 }
