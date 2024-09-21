4f36d3b381fa8230d6c32b0fea2827c1be44a2d3
VS <sofronov.vit@gmail.com>
2024-09-21 13:18:47 +0400

    tags page initial

--- src/app/pages/tags-page/tags-page.component.ts
+++ src/app/pages/tags-page/tags-page.component.ts
@@ -1,13 +1,23 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { TuiHeader } from '@taiga-ui/layout';
+import { TuiTitle } from '@taiga-ui/core';
+import { Store } from '@ngrx/store';
+import { selectTags } from '@store/tags/tag.selectors';
+import { ReplaySubject, takeUntil } from 'rxjs';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { LoaderComponent } from '@components/loader/loader.component';
+import { EmptyListComponent } from '@components/empty-list/empty-list.component';
 
 @Component({
   selector: 'app-tags-page',
   standalone: true,
-  imports: [],
+  imports: [TuiHeader, TuiTitle, NgIf, AsyncPipe, LoaderComponent, EmptyListComponent],
   templateUrl: './tags-page.component.html',
   styleUrl: './tags-page.component.less',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TagsPageComponent {
-
+  private readonly store = inject(Store);
+  private readonly destroy$ = new ReplaySubject(1);
+  tags$ = this.store.select(selectTags).pipe(takeUntil(this.destroy$));
 }
