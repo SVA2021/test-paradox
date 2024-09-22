e6438745392bf900341e9283e308c01ab1169dc6
VS <sofronov.vit@gmail.com>
2024-09-22 11:02:55 +0400

    fix store selectors

--- src/app/store/notices/notice.selectors.ts
+++ src/app/store/notices/notice.selectors.ts
@@ -1,6 +1,6 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { NoticeState } from '@store/notices/notice.reducer';
 
-const selectNoticesFeature = createFeatureSelector<NoticeState>('notice');
+const selectNoticesFeature = createFeatureSelector<NoticeState>('notices');
 
 export const selectNotices = createSelector(selectNoticesFeature, (state: NoticeState) => state);
--- src/app/store/reminders/reminder.selectors.ts
+++ src/app/store/reminders/reminder.selectors.ts
@@ -1,6 +1,6 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { ReminderState } from '@store/reminders/reminder.reducer';
 
-const selectRemindersFeature = createFeatureSelector<ReminderState>('reminder');
+const selectRemindersFeature = createFeatureSelector<ReminderState>('reminders');
 
 export const selectReminders = createSelector(selectRemindersFeature, (state: ReminderState) => state);
