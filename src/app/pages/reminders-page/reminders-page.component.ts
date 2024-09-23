23b2df625d0482a254ccf9c11beb18ec60d81bb0
VS <sofronov.vit@gmail.com>
2024-09-23 09:22:52 +0400

    add alerts for reminders

--- src/app/pages/reminders-page/reminders-page.component.ts
+++ src/app/pages/reminders-page/reminders-page.component.ts
@@ -1,7 +1,7 @@
-import { ChangeDetectionStrategy, Component, inject, INJECTOR, OnDestroy } from '@angular/core';
-import { TuiButton, TuiDialogService, TuiTitle } from '@taiga-ui/core';
+import { ChangeDetectionStrategy, Component, inject, INJECTOR, OnDestroy, OnInit } from '@angular/core';
+import { TuiAlertService, TuiButton, TuiDialogService, TuiTitle } from '@taiga-ui/core';
 import { Store } from '@ngrx/store';
-import { ReplaySubject, takeUntil } from 'rxjs';
+import { combineLatest, interval, ReplaySubject, takeUntil } from 'rxjs';
 import { PolymorpheusComponent } from '@taiga-ui/polymorpheus';
 import { selectReminders } from '@store/reminders/reminder.selectors';
 import { ReminderFormComponent } from '@pages/reminders-page/components/reminder-form/reminder-form.component';
@@ -30,12 +30,34 @@ import { ReminderCardComponent } from '@pages/reminders-page/components/reminder
   styleUrl: './reminders-page.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RemindersPageComponent implements OnDestroy {
+export class RemindersPageComponent implements OnInit, OnDestroy {
+  private readonly alerts = inject(TuiAlertService);
   private readonly dialogs = inject(TuiDialogService);
   private readonly injector = inject(INJECTOR);
   private readonly store = inject(Store);
   private readonly destroy$ = new ReplaySubject(1);
   reminders$ = this.store.select(selectReminders).pipe(takeUntil(this.destroy$));
+
+  ngOnInit() {
+    combineLatest([interval(10000), this.reminders$])
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(([, reminders]) => {
+        const nowTime = Date.now();
+        const warningReminders = reminders.filter(
+          (w) => w.withAlarm && w.timestamp < nowTime && nowTime - w.timestamp < 60000,
+        );
+        warningReminders.forEach((w) => {
+          this.alerts
+            .open(w.description, {
+              label: w.title,
+              autoClose: 9000,
+              appearance: nowTime - w.timestamp <= 10000 ? 'warning' : 'error',
+            })
+            .pipe(takeUntil(this.destroy$))
+            .subscribe();
+        });
+      });
+  }
 
   ngOnDestroy() {
     this.destroy$.next(null);
